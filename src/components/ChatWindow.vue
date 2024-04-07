dd6345c6c329d0a4a383bb3197a3c1ec9a127223
yekhantsan <yekhantsan.dev@gmail.com>
2024-04-07 11:31:50 +0630

    auto scrolling feature

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="chat-window">
-    <div class="messages" v-for="message in messages" :key="message.id">
-      <div class="single">
-        <span class="created-at">{{ message.created_at.toDate() }}</span>
+    <div class="messages" ref="msgBox">
+      <div
+        class="single"
+        v-for="message in formattedMessages"
+        :key="message.id"
+      >
+        <span class="created-at">{{ message.created_at }}</span>
         <span class="name">{{ message.name }}</span>
         <span class="message">{{ message.message }}</span>
       </div>
@@ -11,26 +15,40 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { computed, onUpdated, ref } from "vue";
 import { db } from "../firebase/config";
+import { formatDistanceToNow } from "date-fns";
 
 export default {
   setup() {
     let messages = ref([]);
+    let formattedMessages = computed(() => {
+      return messages.value.map((message) => {
+        let formatTime = formatDistanceToNow(message.created_at.toDate());
+        return { ...message, created_at: formatTime };
+      });
+    });
+
     db.collection("messages")
       .orderBy("created_at")
       .onSnapshot((snap) => {
         let results = [];
         snap.docs.forEach((doc) => {
           let document = { ...doc.data(), id: doc.id };
-          if(doc.data().created_at){
+          if (doc.data().created_at) {
             results.push(document);
           }
         });
         messages.value = results;
       });
 
-    return { messages };
+    // auto scrolling feature
+    let msgBox = ref(null);
+    onUpdated(() => {
+      msgBox.value.scrollTop = msgBox.value.scrollHeight;
+    });
+
+    return { messages, formattedMessages, msgBox };
   },
 };
 </script>
